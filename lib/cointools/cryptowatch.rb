3a96efc8e1194f6dc16bd4925f87eb3131e54262
Kuba Suder <jakub.suder@gmail.com>
2018-01-22 20:20:45 +0100

    rewritten cryptowatch code to use Net::HTTP

--- lib/cointools/cryptowatch.rb
+++ lib/cointools/cryptowatch.rb
@@ -1,12 +1,11 @@
 require_relative 'version'
 
 require 'json'
-require 'open-uri'
 
 module CoinTools
   class Cryptowatch
     BASE_URL = "https://api.cryptowat.ch/markets"
-    HEADERS = { 'User-Agent' => "cointools #{CoinTools::VERSION}" }
+    USER_AGENT = "cointools/#{CoinTools::VERSION}"
 
     class DataPoint
       attr_reader :price, :time
@@ -17,14 +16,35 @@ module CoinTools
       end
     end
 
+    class Exception < StandardError
+      attr_reader :response
+
+      def initialize(response)
+        @response = response
+      end
+    end
+
     def get_price(exchange, market, time = nil)
       unixtime = time.to_i
-      data = open("#{BASE_URL}/#{exchange}/#{market}/ohlc?after=#{unixtime}&periods=300", HEADERS).read
-      json = JSON.load(data)
-      timestamp, o, h, l, c, volume = json['result']['300'].detect { |r| r[0] >= unixtime }
-      actual_time = Time.at(timestamp)
+      url = URI("#{BASE_URL}/#{exchange}/#{market}/ohlc?after=#{unixtime}&periods=300")
 
-      return DataPoint.new(o, actual_time)
+      response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+        request = Net::HTTP::Get.new(url)
+        request['User-Agent'] = USER_AGENT
+
+        http.request(request)
+      end
+
+      case response
+      when Net::HTTPSuccess
+        json = JSON.load(response.body)
+        timestamp, o, h, l, c, volume = json['result']['300'].detect { |r| r[0] >= unixtime }
+        actual_time = Time.at(timestamp)
+        return DataPoint.new(o, actual_time)
+      else
+        raise Exception.new(response)
+      end
+
     # rescue OpenURI::HTTPError => e
       # puts "Connection error or coin not found: #{e}"
     end
