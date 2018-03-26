fb0400a5c7b26ce85b466c0614ccc03c42185e2c
Kuba Suder <jakub.suder@gmail.com>
2018-03-26 18:43:01 +0200

    added first tests 💪

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "cointools"
 
+require 'json'
+require 'webmock/rspec'
+
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
+end
+
+def json(hash)
+  JSON.generate(hash)
 end
