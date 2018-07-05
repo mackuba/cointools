d20fd89c5062bfd1dd78743d88d7d2f175b3c207
Kuba Suder <jakub.suder@gmail.com>
2018-07-05 20:09:17 +0200

    switched CoinMarketCap to API v2

--- lib/cointools/coinmarketcap.rb
+++ lib/cointools/coinmarketcap.rb
@@ -18,17 +18,84 @@ module CoinTools
       'ZAR'
     ]
 
+    Listing = BaseStruct.make(:numeric_id, :name, :symbol, :text_id)
     DataPoint = BaseStruct.make(:time, :usd_price, :btc_price, :converted_price)
 
+    def symbol_map
+      load_listings if @symbol_map.nil?
+      @symbol_map
+    end
+
+    def id_map
+      load_listings if @id_map.nil?
+      @id_map
+    end
+
+    def load_listings
+      url = URI("#{BASE_URL}/v2/listings/")
+
+      response = make_request(url)
+
+      case response
+      when Net::HTTPSuccess
+        json = JSON.load(response.body)
+        raise JSONError.new(response) unless json.is_a?(Hash) && json['data'] && json['metadata']
+        raise BadRequestError.new(response, json['metadata']['error']) if json['metadata']['error']
+        raise JSONError.new(response) unless json['data'].is_a?(Array)
+
+        @id_map = {}
+        @symbol_map = {}
+
+        json['data'].each do |record|
+          listing = Listing.new(
+            numeric_id: record['id'], 
+            name: record['name'],
+            symbol: record['symbol'],
+            text_id: record['website_slug']
+          )
+
+          # TODO: JSONError vs. NoDataError? + error class docs
+          unless listing.numeric_id && listing.name && listing.symbol && listing.text_id
+            raise JSONError.new(response, "Missing field in record: #{record}")
+          end
+
+          @id_map[listing.text_id] = listing
+          @symbol_map[listing.symbol] = listing
+        end
+      when Net::HTTPClientError
+        raise BadRequestError.new(response)
+      else
+        raise ServiceUnavailableError.new(response)
+      end
+    end
 
     def get_price(coin_name, convert_to: nil)
       raise InvalidSymbolError if coin_name.to_s.empty?
 
-      url = URI("#{BASE_URL}/v1/ticker/#{coin_name}/")
+      listing = id_map[coin_name.to_s]
+      raise InvalidSymbolError if listing.nil?
+
+      get_price_for_listing(listing, convert_to: convert_to)
+    end
+
+    def get_price_by_symbol(coin_symbol, convert_to: nil)
+      raise InvalidSymbolError if coin_symbol.to_s.empty?
+
+      listing = symbol_map[coin_symbol.to_s]
+      raise InvalidSymbolError if listing.nil?
+
+      get_price_for_listing(listing, convert_to: convert_to)
+    end
+
+    def get_price_for_listing(listing, convert_to: nil)
+      url = URI("#{BASE_URL}/v2/ticker/#{listing.numeric_id}/")
 
       if convert_to
-        validate_fiat_currency(convert_to)
-        url += "?convert=#{convert_to}"
+        currency = convert_to.upcase
+        validate_fiat_currency(currency)
+        url.query = "convert=#{currency}"
+      else
+        url.query = "convert=BTC"
       end
 
       response = make_request(url)
@@ -36,19 +103,21 @@ module CoinTools
       case response
       when Net::HTTPSuccess
         json = JSON.load(response.body)
-        raise JSONError.new(response) unless json.is_a?(Array)
+        raise JSONError.new(response) unless json.is_a?(Hash) && json['data'] && json['metadata']
+        raise BadRequestError.new(response, json['metadata']['error']) if json['metadata']['error']
 
-        record = json[0]
+        record = json['data']
+        raise JSONError.new(response) unless record.is_a?(Hash)
 
-        usd_price = record['price_usd']&.to_f
-        btc_price = record['price_btc']&.to_f
+        quotes = record['quotes']
+        raise JSONError.new(response, 'Missing quotes field') unless quotes
+
+        usd_price = quotes['USD'] && quotes['USD']['price']&.to_f
+        btc_price = quotes['BTC'] && quotes['BTC']['price']&.to_f
         timestamp = Time.at(record['last_updated'].to_i)
 
-        raise NoDataError.new(response) unless usd_price && btc_price && record['last_updated']
-
-        if convert_to
-          converted_price = record["price_#{convert_to.downcase}"]&.to_f
-          raise NoDataError.new(response, 'Conversion to chosen fiat currency failed') if converted_price.nil?
+        if currency
+          converted_price = quotes[currency] && quotes[currency]['price']&.to_f
         end
 
         return DataPoint.new(
@@ -59,51 +128,6 @@ module CoinTools
         )
       when Net::HTTPNotFound
         raise UnknownCoinError.new(response)
-      when Net::HTTPClientError
-        raise BadRequestError.new(response)
-      else
-        raise ServiceUnavailableError.new(response)
-      end
-    end
-
-    def get_price_by_symbol(coin_symbol, convert_to: nil)
-      raise InvalidSymbolError if coin_symbol.to_s.empty?
-
-      url = URI("#{BASE_URL}/v1/ticker/?limit=0")
-      symbol = coin_symbol.downcase
-
-      if convert_to
-        validate_fiat_currency(convert_to)
-        url.query += "&convert=#{convert_to}"
-      end
-
-      response = make_request(url)
-
-      case response
-      when Net::HTTPSuccess
-        json = JSON.load(response.body)
-        raise JSONError.new(response) unless json.is_a?(Array)
-
-        record = json.detect { |r| r['symbol'].downcase == symbol }
-        raise UnknownCoinError.new(response) if record.nil?
-
-        usd_price = record['price_usd']&.to_f
-        btc_price = record['price_btc']&.to_f
-        timestamp = Time.at(record['last_updated'].to_i)
-
-        raise NoDataError.new(response) unless usd_price && btc_price && timestamp
-
-        if convert_to
-          converted_price = record["price_#{convert_to.downcase}"]&.to_f
-          raise NoDataError.new(response, 'Conversion to chosen fiat currency failed') if converted_price.nil?
-        end
-
-        return DataPoint.new(
-          time: timestamp,
-          usd_price: usd_price,
-          btc_price: btc_price,
-          converted_price: converted_price
-        )
       when Net::HTTPClientError
         raise BadRequestError.new(response)
       else
@@ -135,6 +159,7 @@ module CoinTools
           json = JSON.load(response.body)
           raise JSONError.new(response) unless json.is_a?(Hash) && json['data'] && json['metadata']
           raise NoDataError.new(response, json['metadata']['error']) if json['metadata']['error']
+          raise JSONError.new(response) unless json['data'].is_a?(Array)
 
           json['data'].each do |record|
             quotes = record['quotes']
